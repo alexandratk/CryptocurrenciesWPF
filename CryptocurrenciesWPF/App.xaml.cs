981162a0b6c756c9318dff696ce50233cee1aede
Oleksandra Tkachenko <sanya.t07122001@gmail.com>
2023-10-23 15:54:56 +0300

    localization

--- CryptocurrenciesWPF/App.xaml.cs
+++ CryptocurrenciesWPF/App.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -14,7 +15,11 @@ namespace CryptocurrenciesWPF
     /// </summary>
     public partial class App : Application
     {
-        public App() { }
+        public App() {
+            //System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("ru-RU");
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("uk-UA");
+            //System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en");
+        }
 
         protected override void OnStartup(StartupEventArgs e)
         {
